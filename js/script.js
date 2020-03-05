d1ccc511b1f3723a77f9ec5d92d9aa8ead31f13d
nos <nos@kmc.gr.jp>
2020-03-05 17:25:00 +0900

    add markerFound Event

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 var scene = document.querySelector("a-scene");
+AFRAME.registerComponent('registerevents', {
+    init: function () {
+        var marker = this.el;
+
+        marker.addEventListener('markerFound', function() {
+            var markerId = marker.id;
+            console.log('markerFound', markerId);
+            // TODO: Add your own code here to react to the marker being found.
+            
+        });
+
+        marker.addEventListener('markerLost', function() {
+            var markerId = marker.id;
+            console.log('markerLost', markerId);
+            // TODO: Add your own code here to react to the marker being lost.
+        });
+    }
+});
