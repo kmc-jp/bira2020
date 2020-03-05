795cf74b4f5d62b635d3cfb5e4bd609db8d0a9d0
nos <nos@kmc.gr.jp>
2020-03-05 21:27:32 +0900

    add animation etc

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 var scene = document.querySelector("a-scene");
 AFRAME.registerComponent('registerevents', {
     init: function () {
+        console.log("aaaaaaaaaaaaaaa")
         var marker = this.el;
-
         marker.addEventListener('markerFound', function() {
             var markerId = marker.id;
             console.log('markerFound', markerId);
             // TODO: Add your own code here to react to the marker being found.
-            
+            // document.querySelector("#bira1").emit("fade")
+            Array.from(document.querySelectorAll("[id^='bira']"),  e => {e.emit("fade")})
         });
 
         marker.addEventListener('markerLost', function() {
