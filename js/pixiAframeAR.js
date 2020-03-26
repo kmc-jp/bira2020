6d2b1c5dd0a376e54ef6a2b5bc86f6cf4e09f965
nos <nos@kmc.gr.jp>
2020-03-27 00:07:43 +0900

    位置調整とか

--- js/pixiAframeAR.js
+++ js/pixiAframeAR.js
@@ -109,7 +109,7 @@ window.onload = function () {
 		plane.setAttribute('height', '10');
 		plane.setAttribute('width', '10');
 		//マーカーを基準にしたモデルの相対位置
-		plane.setAttribute('position', '0 -2 0');
+		plane.setAttribute('position', '0 2 0');
 		var stand = stand_mode ? '0 0 0' : '-90 0 0';
 		plane.setAttribute('rotation', stand);
 		marker.appendChild(plane);
